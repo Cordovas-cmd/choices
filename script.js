698d555ab5c22c519409630a48eb4529a68d16b2
Samuel Cordova <cordovas1337@gmail.com>
2022-11-10 23:44:25 -0600

    added some todos

---
+++ script.js
@@ -0,0 +1,7 @@
+// will add some choices
+
+// will add an option to randomize the choise as well as have an animation to do so
+
+// need to add a possible index of choices as well as some rng or math random functionality?
+
+// need to add an area type a value and add the value to an array.
